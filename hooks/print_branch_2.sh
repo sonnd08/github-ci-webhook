a33bae505e52c6c8d3c5ade386e3da739493b9d9
sonnd08 <sonnd@dgroup.co>
2021-01-04 17:48:43 +0700

    test multiple script

--- hooks/print_branch_2.sh
+++ hooks/print_branch_2.sh
@@ -2,5 +2,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
-echo "Webhook received for branch '$@'" >> ../logs.txt
+echo "Webhook received for branch 23" >> ../logs2.txt
 exit 0
---
+++ hooks/print_branch_3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+echo "Webhook received for branch >> 22'$@'" >> ../logs.txt
+exit 0
