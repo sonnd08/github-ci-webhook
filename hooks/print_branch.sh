bda858f760e25c02d75a9392d7b3daf2f03439fd
sonnd08 <sonnd@dgroup.co>
2021-01-04 17:21:56 +0700

    print logs by relative path

--- hooks/print_branch.sh
+++ hooks/print_branch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo "Webhook received for branch '$1'" >> logs.txt
+cd "$parent_path"
+echo "Webhook received for branch '$1'" >> ../logs.txt
 exit 0
